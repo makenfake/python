9e137e97839a71a4bdf3ea31bbc7e253ba6a6519
Dan Nguyen <dansonguyen@gmail.com>
2016-03-16 18:20:35 -0700

    synced

--- Gemfile
+++ Gemfile
@@ -9,6 +9,14 @@ gem 'wdm', '~> 0.1.0', platforms: [:mswin, :mingw]
 gem 'tzinfo-data', platforms: [:mswin, :mingw, :jruby]
 
 # Middleman Gems
-gem 'middleman', '>= 4.0.0'
+gem 'middleman', '>= 4.1.2'
 gem 'middleman-livereload'
 gem 'middleman-compass', '>= 4.0.0'
+
+gem 'mime-types' # dependency for Fog with s3_sync: https://github.com/fog/fog-core/issues/171
+
+gem 'bootstrap', '~> 4.0.0.alpha3'
+gem "middleman-syntax"
+gem 'middleman-s3_sync', '>= 4.0.2'
+gem "middleman-sprockets", "~> 4.0.0.rc" # take this out in favor of webpack...
+gem 'middleman-pry'
--- config.rb
+++ config.rb
@@ -1,3 +1,30 @@
+
+# General configuration
+
+# Reload the browser automatically whenever files change
+
+
+activate :directory_indexes
+activate :pry
+activate :syntax, :line_numbers => false
+set :trailing_slash, true
+
+set :js_dir, 'assets/javascripts'
+set :css_dir, 'assets/stylesheets'
+set :images_dir, 'assets/images'
+
+# Build-specific configuration
+configure :build do
+  # Minify CSS on build
+  activate :minify_css
+  # Minify Javascript on build
+  activate :minify_javascript
+end
+
+
+configure :development do
+  activate :livereload
+end
 ###
 # Page options, layouts, aliases and proxies
 ###
@@ -8,6 +35,7 @@
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
+page '/*.csv', layout: false
 
 # With alternative layout
 # page "/path/to/file.html", layout: :otherlayout
@@ -16,29 +44,15 @@ page '/*.txt', layout: false
 # proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
 #  which_fake_page: "Rendering a fake page with a local variable" }
 
-# General configuration
-
-# Reload the browser automatically whenever files change
-configure :development do
-  activate :livereload
-end
-
-###
-# Helpers
-###
-
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
-
-# Build-specific configuration
-configure :build do
-  # Minify CSS on build
-  # activate :minify_css
-
-  # Minify Javascript on build
-  # activate :minify_javascript
+activate :s3_sync do |s3_sync|
+  s3_sync.delete                     = false
+  s3_sync.after_build                = true
+  s3_sync.prefer_gzip                = true
+  s3_sync.path_style                 = true
+  s3_sync.reduced_redundancy_storage = false
+  s3_sync.acl                        = 'public-read'
+  s3_sync.encryption                 = false
+  s3_sync.version_bucket             = false
+  s3_sync.index_document             = 'index.html'
+  s3_sync.error_document             = '404.html'
 end
